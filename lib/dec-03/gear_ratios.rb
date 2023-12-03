823cd1a2cbdcdd765f4ed2018dc063bbf8aa771c
martin sereinig <srecnig@veryunited.net>
2023-12-03 12:12:51 +0100

    neighbouring

--- lib/dec-03/gear_ratios.rb
+++ lib/dec-03/gear_ratios.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 module GearRatios
+  Coordinate = Struct.new(:x, :y)
+
   class Schematic
     def initialize(rows)
       @column_count = rows[0].length
       @row_count = rows.length
       @points = Array.new(@row_count) { Array.new(@column_count) }
       rows.each_with_index do |row, y_index|
-        row.each_char.with_index do |char, x_index|
-          @points[y_index][x_index] = Point.new(x: x_index, y: y_index, char:)
+        row.each_char.with_index do |char_, x_index|
+          @points[y_index][x_index] = Point.new(
+            Coordinate.new(x_index, y_index), char: char_
+          )
         end
       end
       build_logical_map!
@@ -26,7 +30,7 @@ module GearRatios
 
     def draw
       @points.each do |row|
-        p row.map(&:logical_char).join
+        puts row.map(&:logical_char).join
       end
     end
   end
@@ -35,11 +39,10 @@ module GearRatios
     attr_accessor :is_number, :is_symbol, :is_blank, :is_adjacent
     attr_reader :x, :y, :char, :logical_char
 
-    def initialize(x:, y:, char:)
-      @x = x
-      @y = y
+    def initialize(coordinate, char:)
+      @x = coordinate.x
+      @y = coordinate.y
       @char = char
-      # @logical_char = char
     end
 
     def apply_logic!
@@ -59,6 +62,20 @@ module GearRatios
         @is_blank = false
         @logical_char = 'X'
       end
+    end
+
+    def neighbours(zero:, max:)
+      coordinates = [
+        Coordinate.new(@x, @y - 1),
+        Coordinate.new(@x + 1, @y - 1),
+        Coordinate.new(@x + 1, @y),
+        Coordinate.new(@x + 1, @y + 1),
+        Coordinate.new(@x, @y + 1),
+        Coordinate.new(@x - 1, @y + 1),
+        Coordinate.new(@x - 1, @y),
+        Coordinate.new(@x - 1, @y - 1)
+      ]
+      coordinates.filter { |n| n.x >= zero.x && n.y >= zero.y && n.x <= max.x && n.y <= max.y }
     end
   end
 end
--- test/dec-03/gear_ratios_test.rb
+++ test/dec-03/gear_ratios_test.rb
@@ -5,8 +5,10 @@ require 'minitest/pride'
 require_relative '../../lib/dec-03/gear_ratios'
 
 class GearRatiosTest < Minitest::Test
+  include GearRatios
+
   def test_initalization
-    schematic = GearRatios::Schematic.new(
+    schematic = Schematic.new(
       [
         '...4',
         '12.$',
@@ -22,25 +24,64 @@ class GearRatiosTest < Minitest::Test
   end
 
   def test_apply_logic
-    number_point = GearRatios::Point.new(x: 0, y: 0, char: '5')
+    number_point = Point.new(Coordinate.new(0, 0), char: '5')
     number_point.apply_logic!
     assert_equal true, number_point.is_number
     assert_equal false, number_point.is_symbol
     assert_equal false, number_point.is_blank
     assert_equal 'N', number_point.logical_char
 
-    blank_point = GearRatios::Point.new(x: 0, y: 0, char: '.')
+    blank_point = Point.new(Coordinate.new(0, 0), char: '.')
     blank_point.apply_logic!
     assert_equal false, blank_point.is_number
     assert_equal false, blank_point.is_symbol
     assert_equal true, blank_point.is_blank
     assert_equal '.', blank_point.logical_char
 
-    symbol_point = GearRatios::Point.new(x: 0, y: 0, char: '$')
+    symbol_point = Point.new(Coordinate.new(0, 0), char: '$')
     symbol_point.apply_logic!
     assert_equal false, symbol_point.is_number
     assert_equal true, symbol_point.is_symbol
     assert_equal false, symbol_point.is_blank
     assert_equal 'X', symbol_point.logical_char
+  end
+
+  def test_neighbours
+    zero = Coordinate.new(0, 0)
+    max = Coordinate.new(2, 2)
+    p = Point.new(Coordinate.new(0, 0), char: '.')
+    neighbours = p.neighbours(zero:, max:)
+    assert_equal true, neighbours.any?(Coordinate.new(1, 0))
+    assert_equal true, neighbours.any?(Coordinate.new(1, 1))
+    assert_equal true, neighbours.any?(Coordinate.new(0, 1))
+    assert_equal 3, neighbours.length
+
+    p = Point.new(Coordinate.new(2, 2), char: '.')
+    neighbours = p.neighbours(zero:, max:)
+    assert_equal true, neighbours.any?(Coordinate.new(1, 2))
+    assert_equal true, neighbours.any?(Coordinate.new(1, 1))
+    assert_equal true, neighbours.any?(Coordinate.new(2, 1))
+    assert_equal 3, neighbours.length
+
+    p = Point.new(Coordinate.new(1, 1), char: '.')
+    neighbours = p.neighbours(zero:, max:)
+    assert_equal true, neighbours.any?(Coordinate.new(1, 0))
+    assert_equal true, neighbours.any?(Coordinate.new(2, 0))
+    assert_equal true, neighbours.any?(Coordinate.new(2, 1))
+    assert_equal true, neighbours.any?(Coordinate.new(2, 2))
+    assert_equal true, neighbours.any?(Coordinate.new(1, 2))
+    assert_equal true, neighbours.any?(Coordinate.new(0, 2))
+    assert_equal true, neighbours.any?(Coordinate.new(0, 1))
+    assert_equal true, neighbours.any?(Coordinate.new(0, 0))
+    assert_equal 8, neighbours.length
+
+    p = Point.new(Coordinate.new(0, 1), char: '.')
+    neighbours = p.neighbours(zero:, max:)
+    assert_equal true, neighbours.any?(Coordinate.new(0, 0))
+    assert_equal true, neighbours.any?(Coordinate.new(1, 0))
+    assert_equal true, neighbours.any?(Coordinate.new(1, 1))
+    assert_equal true, neighbours.any?(Coordinate.new(1, 2))
+    assert_equal true, neighbours.any?(Coordinate.new(0, 2))
+    assert_equal 5, neighbours.length
   end
 end
