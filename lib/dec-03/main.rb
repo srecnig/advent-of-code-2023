d0ac1f5bbe0e4a2188d209a5fcaec32c19aa6057
martin sereinig <srecnig@veryunited.net>
2023-12-03 09:45:18 +0100

    basic setup

---
+++ lib/dec-03/main.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'gear_ratios'
+
+def main1(_filename)
+  p "hello world #{filename}"
+end
+
+main1('input1.txt')
---
+++ test/dec-03/gear_ratios_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../../lib/dec-03/gear_ratios'
+
+class GearRatiosTest < Minitest::Test
+  def test_something
+    assert_equal 4, GearRatios.testor
+  end
+end
