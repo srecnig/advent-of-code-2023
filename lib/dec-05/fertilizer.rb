0f0d1334b5239834bacd5ef06bb4f0dceb7c9526
martin sereinig <srecnig@veryunited.net>
2023-12-05 22:04:22 +0100

    part1!

--- lib/dec-05/fertilizer.rb
+++ lib/dec-05/fertilizer.rb
@@ -23,27 +23,31 @@ module Fertilizer
     end
   end
 
+  AlamancRange = Struct.new(:range, :destination_start)
+
   class AlmanacMap
     attr_reader :source, :destination
 
     def initialize(source:, destination:, range_strings:)
-      p "source: #{source}, destination: #{destination}, range_strings: #{range_strings}"
-
       @source = source
       @destination = destination
-      @mapping = {}
+      @input_ranges = []
 
       range_strings.map(&:split).map { |d, s, l| [d.to_i, s.to_i, l.to_i] }.each do |dest_start, src_start, length|
         raise "length == 0, this shouldn't happen." if length.zero?
 
-        length.times do |i|
-          @mapping[src_start + i] = dest_start + i
-        end
+        @input_ranges << AlamancRange.new((src_start...(src_start + length)), dest_start)
       end
     end
 
     def [](key)
-      @mapping.fetch(key, key)
+      @input_ranges.each do |input_range|
+        if input_range.range.include?(key)
+          position_in_range = key - input_range.range.begin
+          return input_range.destination_start + position_in_range
+        end
+      end
+      key
     end
   end
 end
