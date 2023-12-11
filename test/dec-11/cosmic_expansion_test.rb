7bf98422672b42387e9af3999f4456bc4884f9d2
martin sereinig <srecnig@veryunited.net>
2023-12-11 06:52:12 +0100

    setup

---
+++ test/dec-11/cosmic_expansion_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../../lib/dec-11/cosmic_expansion'
+
+class CosmicExpansionTest < Minitest::Test
+  include CosmicExpansion
+end
