31c4a53ae91c06034322bf54351156e5635ea694
martin sereinig <srecnig@veryunited.net>
2023-12-11 08:05:03 +0100

    expansion

--- lib/dec-11/cosmic_expansion.rb
+++ lib/dec-11/cosmic_expansion.rb
@@ -1,4 +1,89 @@
 # frozen_string_literal: true
 
 module CosmicExpansion
+  Coordinate = Struct.new(:x, :y)
+
+  Point = Struct.new(:coordinate, :char) do
+    def empty_space?
+      char == '.'
+    end
+
+    def galaxy?
+      char == '#'
+    end
+  end
+
+  class Galaxy
+    attr_reader :points, :row_count, :column_count, :galaxy_points
+
+    def initialize(lines)
+      @column_count = lines[0].length
+      @row_count = lines.length
+      @points = Array.new(@row_count) { Array.new(@column_count) }
+      lines.each_with_index do |row, y_index|
+        row.each_char.with_index do |char, x_index|
+          @points[y_index][x_index] = Point.new(
+            Coordinate.new(x_index, y_index), char
+          )
+        end
+      end
+      expand_vertically!
+      expand_horizontally!
+      remap!
+    end
+
+    def [](coordinate)
+      @points[coordinate.y][coordinate.x]
+    end
+
+    def expand_vertically!
+      add_y_index = []
+      @points.each.with_index do |row, y|
+        add_y_index << y if row.all?(&:empty_space?)
+      end
+      add_y_index.reverse.each do |y|
+        new_array = Array.new(@column_count) { |x| Point.new(Coordinate.new(x, y - 1), '.') }
+        @points.insert(y, new_array)
+      end
+      @row_count += add_y_index.length
+    end
+
+    def expand_horizontally!
+      add_x_index = []
+      (0...@column_count).each do |x|
+        column_empty = (0...@row_count).all? { |y| self[Coordinate.new(x, y)].empty_space? }
+        add_x_index << x if column_empty
+      end
+      add_x_index.reverse.each do |x|
+        @points.each.with_index do |row, y|
+          row.insert(x, Point.new(Coordinate.new(x - 1, y), '.'))
+        end
+      end
+      @column_count += add_x_index.length
+    end
+
+    def remap!
+      # coordinates in points are out of sync with actual coordinates. fix.
+      @galaxy_coordinates = []
+      (0...@row_count).each do |y|
+        (0...@column_count).each do |x|
+          real_coordinate = Coordinate.new(x, y)
+          current = self[real_coordinate]
+          # replace with point that for sure has right coordinates + existing char
+          new_point = Point.new(real_coordinate, current.char)
+          @points[y][x] = new_point
+          @galaxy_coordinates << new_point if new_point.galaxy?
+        end
+      end
+    end
+  end
 end
+
+# @points = Array.new(row_count) { Array.new(column_count) }
+# rows.each_with_index do |row, y_index|
+#   row.each_char.with_index do |char_, x_index|
+#     @points[y_index][x_index] = Point.new(
+#       Coordinate.new(x_index, y_index), char: char_
+#     )
+#   end
+# end
--- test/dec-11/cosmic_expansion_test.rb
+++ test/dec-11/cosmic_expansion_test.rb
@@ -6,4 +6,47 @@ require_relative '../../lib/dec-11/cosmic_expansion'
 
 class CosmicExpansionTest < Minitest::Test
   include CosmicExpansion
+
+  def test_expansion
+    lines = [
+      #  |     |
+      '...#......',
+      '.......#..',
+      '#.........',
+      '..........', # - 3
+      '......#...',
+      '.#........',
+      '.........#',
+      '..........', # - 7
+      '.......#..',
+      '#...#.....'
+    ]
+    galaxy = Galaxy.new(lines)
+    assert_equal 12, galaxy.row_count
+    assert_equal 13, galaxy.column_count
+    # these vertical lines were added
+    assert_equal true, galaxy.points[3].all?(&:empty_space?)
+    assert_equal true, galaxy.points[4].all?(&:empty_space?)
+    assert_equal true, galaxy.points[8].all?(&:empty_space?)
+    assert_equal true, galaxy.points[9].all?(&:empty_space?)
+    # columns have been added, which made the galaxies move
+    assert_equal true, galaxy[Coordinate.new(12, 7)].galaxy?
+
+    p 'aaaah'
+    galaxy.points.each do |row|
+      p(row.map { |p| "(#{p.coordinate.x}, #{p.coordinate.y})" }.join(' '))
+    end
+    p 'bbbbh'
+    galaxy.points.each do |row|
+      p(row.map(&:char).join(' '))
+    end
+  end
+
+  def test_point
+    point1 = Point.new(Coordinate.new(0, 0), '.')
+    assert_equal true, point1.empty_space?
+
+    point2 = Point.new(Coordinate.new(1, 1), '#')
+    assert_equal true, point2.galaxy?
+  end
 end
