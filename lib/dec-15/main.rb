b8291fe8386fc88dd568caea206795bc9a43ea51
martin sereinig <srecnig@veryunited.net>
2023-12-16 00:13:05 +0100

    pt1 only

--- lib/dec-15/main.rb
+++ lib/dec-15/main.rb
@@ -3,8 +3,9 @@
 require_relative 'lens_library'
 
 def main1(filepath)
-  something = File.readlines(File.join(File.dirname(__FILE__), filepath), chomp: true)
-  p something
+  single_line = File.readlines(File.join(File.dirname(__FILE__), filepath), chomp: true)
+  result = single_line[0].split(',').inject(0) { |r, str| r + LensLibrary.hash(str) }
+  p result
 end
 
 def main2(filepath)
@@ -12,5 +13,5 @@ def main2(filepath)
   p something
 end
 
-main1('input1.txt')
+main1('input.txt')
 # main2('input1.txt')
--- test/dec-15/lens_library_test.rb
+++ test/dec-15/lens_library_test.rb
@@ -6,4 +6,11 @@ require_relative '../../lib/dec-15/lens_library'
 
 class LensLibraryTest < Minitest::Test
   include LensLibrary
+
+  def test_hash
+    assert_equal 52, LensLibrary.hash('HASH')
+    assert_equal 30, LensLibrary.hash('rn=1')
+    assert_equal 214, LensLibrary.hash('pc=6')
+    assert_equal 231, LensLibrary.hash('ot=7')
+  end
 end
