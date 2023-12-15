570b623c912a337f38eb5d411a3d7acd318d42e5
martin sereinig <srecnig@veryunited.net>
2023-12-15 23:57:42 +0100

    setup

---
+++ test/dec-15/lens_library_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../../lib/dec-15/lens_library'
+
+class LensLibraryTest < Minitest::Test
+  include LensLibrary
+end
