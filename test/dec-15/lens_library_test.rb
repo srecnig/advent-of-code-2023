3f33620a9574a04986697de4a30e6b8b3868fd13
martin sereinig <srecnig@veryunited.net>
2023-12-16 10:49:41 +0100

    dec-15 pt2 (#17)
    pt2 was more tedious than anticipated.

--- test/dec-15/lens_library_test.rb
+++ test/dec-15/lens_library_test.rb
@@ -13,4 +13,26 @@ class LensLibraryTest < Minitest::Test
     assert_equal 214, LensLibrary.hash('pc=6')
     assert_equal 231, LensLibrary.hash('ot=7')
   end
+
+  def test_hash_map
+    hash_map = HashMap.new
+    lens = Lens.new('rn', 1)
+    hash_map.operate!(lens, :set)
+    assert_equal [Lens.new('rn', 1)], hash_map['rn'].content
+    lens = Lens.new('cm', nil)
+    hash_map.operate!(lens, :remove)
+    assert_equal [Lens.new('rn', 1)], hash_map['rn'].content
+    lens = Lens.new('qp', 3)
+    hash_map.operate!(lens, :set)
+    assert_equal [Lens.new('rn', 1)], hash_map['rn'].content
+    assert_equal [Lens.new('qp', 3)], hash_map['qp'].content
+    lens = Lens.new('cm', 2)
+    hash_map.operate!(lens, :set)
+    assert_equal [Lens.new('rn', 1), Lens.new('cm', 2)], hash_map['rn'].content
+    assert_equal [Lens.new('rn', 1), Lens.new('cm', 2)], hash_map['cm'].content
+    assert_equal [Lens.new('qp', 3)], hash_map['qp'].content
+    assert_equal 1, hash_map.focusing_power('rn')
+    assert_equal 4, hash_map.focusing_power('cm')
+    assert_equal 6, hash_map.focusing_power('qp')
+  end
 end
