7aded97b4c64006c898c915dbe6e314eb640ca12
martin sereinig <srecnig@veryunited.net>
2023-12-16 10:46:33 +0100

    pt2

--- lib/dec-15/lens_library.rb
+++ lib/dec-15/lens_library.rb
@@ -8,4 +8,44 @@ module LensLibrary
       current_value % 256
     end
   end
+
+  Box = Struct.new(:multiplier, :content)
+  Lens = Struct.new(:label, :focal_length)
+
+  class HashMap
+    def initialize
+      @boxes = {}
+      256.times do |i|
+        @boxes[i] = Box.new(i + 1, [])
+      end
+    end
+
+    def [](key)
+      @boxes[LensLibrary.hash(key)]
+    end
+
+    def operate!(lens, operation)
+      box = @boxes[LensLibrary.hash(lens.label)]
+      lens_locations = box.content.each.with_index.with_object([]) do |(l, i), locations|
+        locations << i if l.label == lens.label
+      end
+      if operation == :remove
+        lens_locations.reverse.each { |index| box.content.delete_at(index) }
+      elsif lens_locations.empty?
+        box.content << lens
+      else
+        lens_locations.each { |index| box.content[index] = lens }
+      end
+    end
+
+    def focusing_power(key)
+      box = @boxes[LensLibrary.hash(key)]
+      box_multiplier = box.multiplier
+      key_index = box.content.find_index { |l| l.label == key }
+      slot_multiplier = key_index.nil? ? 0 : key_index + 1
+      focal_length = key_index.nil? ? 0 : box.content[slot_multiplier - 1].focal_length
+
+      box_multiplier * slot_multiplier * focal_length
+    end
+  end
 end
--- lib/dec-15/main.rb
+++ lib/dec-15/main.rb
@@ -9,9 +9,27 @@ def main1(filepath)
 end
 
 def main2(filepath)
-  something = File.readlines(File.join(File.dirname(__FILE__), filepath), chomp: true)
-  p something
+  single_line = File.readlines(File.join(File.dirname(__FILE__), filepath), chomp: true)
+  hash_map = LensLibrary::HashMap.new
+  operations = single_line[0].split(',').map do |op_string|
+    op = op_string[-1] == '-' ? :remove : :set
+    if op == :remove
+      key = op_string[..-2]
+      value = nil
+    else
+      set_arr = op_string.split('=')
+      key = set_arr[0]
+      value = set_arr[1].to_i
+    end
+    [LensLibrary::Lens.new(key, value), op]
+  end
+  operations.each { |(lens, op)| hash_map.operate!(lens, op) }
+  # ||   # .inject(0) { |_op| [] }
+  result = operations.map { |(lens, _op)| lens.label }.uniq.inject(0) do |r, key|
+    r + hash_map.focusing_power(key)
+  end
+  p result
 end
 
 main1('input.txt')
-# main2('input1.txt')
+main2('input.txt')
