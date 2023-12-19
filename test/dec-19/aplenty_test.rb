32a8b801b095b06f18183af926667c3a5c3545d3
martin sereinig <srecnig@veryunited.net>
2023-12-19 08:53:19 +0100

    setup

---
+++ test/dec-19/aplenty_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../../lib/dec-19/aplenty'
+
+class AplentyTest < Minitest::Test
+  include Aplenty
+end
